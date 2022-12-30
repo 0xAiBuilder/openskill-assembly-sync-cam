0e4ae802b761ae34cd3e20b30faca6da448c2a42
0xAiBuilder <115203448+0xAiBuilder@users.noreply.github.com>
2022-12-30 14:35:22 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -14,7 +14,8 @@ const fs = require('fs');
 const io = new Server(server, {
     cors: {
         //origin: "http://localhost:3000",
-        origin: "https://openskill-sync-cam.web.app",
+        //origin: "https://openskill-sync-cam.web.app",
+        origin: "*",
         methods: ["GET", "POST"],
     },
 });
